3f12da80b01717b91f5c28610a002f05fa30645e
OSIFO  FAMOUS <70143479+famous10@users.noreply.github.com>
2022-08-24 16:20:10 +0100

    my redux application

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector,useDispatch } from 'react-redux/es/exports';
+import './App.css';
+import {actions} from './store/index'
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch()
+  const increment = () => {
+     dispatch(actions.increment());
+  }
+  const decrement = () => {
+    dispatch(actions.decrement());
+  } 
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  } 
+  return (
+    <div>
+     <h1>Counter App </h1>
+     <h2>{counter}</h2>
+     <button onClick={increment}> increment</button>
+     <button onClick={decrement}>Decrement</button>
+     <button onClick={addBy}>Add Value by 10</button>
+    </div>
+  );
+}
+
+export default App;
